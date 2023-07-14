0623267d36cee08668c9d99754ccc1775133dd06
AmanHogan <ahoganbailey@gmail.com>
2023-07-14 14:21:19 -0500

    Npm file updates

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const PORT_NUMBER = 3000;
+const fetch = require('node-fetch');
+const multer = require('multer');
 
 
 // Set view engine and static public pages
 app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+
+
+
+
 
 // Render First Home Page
 app.get('/', (req, res) =>
@@ -13,12 +21,40 @@ app.get('/', (req, res) =>
 })
 
 
+app.get('/Skills', (req, res) => {
+    res.render('Skills')
+})
 
-// Links router with routes
-const userRouter = require('./routes/sections')
-app.use('/sections', userRouter)
+app.get('/WorkExperience', (req, res) => {
+    res.render('WorkExperience')
+})
 
-app.listen(3000)
+app.get('/FinalView', (req, res) => {
+    res.render('FinalView')
+})
+
+app.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+app.get('/pythonFile', (req, res) => {
+    const flaskUrl = 'http://localhost:5000/pythonFile'; // Flask backend URL
+  
+    fetch(flaskUrl)
+      .then(response => response.json())
+      .then(data => 
+        {
+        res.json(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data from Flask backend:', error);
+        res.status(500).json({ error: 'An error occurred' });
+      });
+  });
+
+
+
+app.listen(PORT_NUMBER)
 
 
 
